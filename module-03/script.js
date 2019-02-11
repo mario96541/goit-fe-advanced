a5a6a03b045277ab8b96b43bff2fde08465bc518
mario96541 <41206432+mario96541@users.noreply.github.com>
2019-02-11 12:46:52 +0200

    add 1v

--- module-03/script.js
+++ module-03/script.js
@@ -1,2 +1,17 @@
 "use strict";
 
+const logins = ["Mango", "robotGoogles", "Poly", "Aj4x1sBozz", "qwerty123"];
+
+const isLoginValid = (login) => login.length >= 4 && login.length <= 16;
+
+const isLoginUnique = (allLogins, login) => allLogins.includes(login);
+
+const addLogin = (allLogins, login) => {
+    if(!isLoginValid(login)) {
+        return 'Такой логин уже используется!';
+    }
+    if(!isLoginUnique(allLogins, login)) {
+        return 'Ошибка! Логин должен быть от 4 до 16 символов';
+    }
+    return 'Логин успешно добавлен!';
+};
